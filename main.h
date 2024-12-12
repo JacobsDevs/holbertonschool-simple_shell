2697f9ac25a6c042c259f9a41f2623d70317af3c
JacobsDevs <jacobsdevsmail@gmail.com>
2024-12-13 10:22:06 +1100

    refactor: Add descriptions and abstract functionality

---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void clean_args(char *args[]);
+void print_args(char *args[]);
+void build_args(char *input, char *argv[]);
+
+#endif
--- simple_shell.c
+++ simple_shell.c
@@ -5,8 +5,17 @@
 #include <string.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include "main.h"
 
 extern char **environ;
+
+/**
+ * main - Main shell process.  Prompts the user for input and then uses
+ * builds an argument list, forks and uses execve to execute the desired
+ * command.
+ *
+ * Return: 0 on successful exit.
+ */
 
 int main(void)
 {
@@ -15,9 +24,6 @@ int main(void)
 	char *buffer;
 	size_t buffer_size = 64;
 	char *argv[4];
-	int i = 0;
-	char *token;
-	char *tmp;
 	struct stat sb;
 
 	while (1)
@@ -30,19 +36,10 @@ int main(void)
 			printf("Exiting\n");
 			break;
 		}
-		token = strtok(input, "\n");
-		token = strtok(token, " ");
-		while (token != NULL)
-		{
-			argv[i] = malloc(strlen(token));
-			strcpy(argv[i], token);
-			token = strtok(NULL, " ");
-			i++;
-		}
-		argv[i] = NULL;
+		build_args(input, argv);
 		if (stat(argv[0], &sb) == -1)
 		{
-			printf("Not found: %s\n", token);
+			printf("Not found: %s\n", argv[0]);
 			continue;
 		}
 		child = fork();
@@ -54,10 +51,67 @@ int main(void)
 				printf("Unable to exec\n");
 			return (0);
 		}
-		else 
+		else
 		{
 			wait(&child);
 		}
+		clean_args(argv);
 	}
-	exit (0);
+	exit(0);
+}
+
+/**
+ * build_args - Build the argv list from the users input.
+ * @input: String the user inputted to the terminal
+ * @argv: The array of string arguments to build.
+ */
+void build_args(char *input, char *argv[])
+{
+	int i = 0;
+	char *token;
+
+	token = strtok(input, "\n");
+	token = strtok(token, " ");
+	while (token != NULL)
+	{
+		argv[i] = malloc(strlen(token));
+		strcpy(argv[i], token);
+		token = strtok(NULL, " ");
+		i++;
+	}
+	argv[i] = NULL;
+}
+
+/**
+ * clean_args - Run after the main loop to clear out the args and prepare
+ * for a new set of args.
+ * @args: The array of string arguments to clear.
+ */
+
+void clean_args(char *args[])
+{
+	int i = 0;
+
+	while (args[i] != NULL)
+	{
+		args[i] = "";
+		i++;
+	}
+	/*print_args(args);*/
+}
+
+/**
+ * print_args - A utility function to check the contents of the args list,
+ * used for debugging.
+ * @args: The array of string arguments to print.
+ */
+void print_args(char *args[])
+{
+	int i = 0;
+
+	while (args[i] != NULL)
+	{
+		printf("arg %d = %s\n", i, args[i]);
+		i++;
+	}
 }

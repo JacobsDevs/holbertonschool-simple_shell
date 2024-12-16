2ec0af1804cb3808d33f56cc6f90108c369ba662
JacobsDevs <jacobsdevsmail@gmail.com>
2024-12-16 12:34:24 +1100

    refactor: simple_shell.c to avoid memory issues later

--- ppid.c
+++
@@ -1,8 +0,0 @@
-#include <unistd.h>
-#include <stdio.h>
-
-int ppid(void)
-{
-	printf("%u\n", getppid());
-	return (0);
-}
--- simple_shell.c
+++ simple_shell.c
@@ -9,6 +9,7 @@
 
 #define MAX_STRING_LENGTH 20
 
+
 /**
  * main - Main shell process.  Prompts the user for input and then uses
  * builds an argument list, forks and uses execve to execute the desired
@@ -20,143 +21,114 @@
 int main(void)
 {
 	pid_t child;
-	char *input;
-	size_t buffer_size = 64;
 	char **argv = NULL;
 	struct stat sb;
+	int count = 3;
+	int running = 1;
 
-	while (1)
+	while (running == 1)
 	{
-		argv = (char **)malloc(sizeof(char *) * 5);
+		argv = malloc_argv(count);
 		if (argv == NULL)
 		{
-			printf("Failed to malloc for argv\n");
+			printf("Failed to malloc argv\n");
 			return (1);
 		}
-		setup_argv(argv, 5);
 		if (isatty(0) == 0)
 		{
-			input = malloc(sizeof(char) * buffer_size);
-			getline(&input, &buffer_size, stdin);
-			build_args(input, argv);
+			get_input(argv);
 			fflush(stdin);
 		}
 		else
 		{
 			printf("$ ");
-			input = malloc(sizeof(char) * buffer_size);
-			getline(&input, &buffer_size, stdin);
-			if (strcmp(input, "exit\n") == 0)
-			{
-				printf("Exiting\n");
-				break;
-			}
-			build_args(input, argv);
+			get_input(argv);
 		}
 		if (stat(argv[0], &sb) == -1)
 		{
-			printf("Not found: %s\n", argv[0]);
+			printf("%s: command not found\n", argv[0]);
+			clean_argv(argv, count);
 			continue;
 		}
+		if (strcmp(argv[0], "exit") == 0)
+			execve(argv[0], argv, environ);
 		child = fork();
 		if (child == -1)
-			printf("Failed to start child\n");
+			printf("Failed to Fork");
 		if (child == 0)
-		{
-			if (execve(argv[0], argv, environ) == -1)
-				printf("Unable to exec\n");
-			return (0);
-		}
+			execve(argv[0], argv, environ);
 		else
 		{
 			wait(&child);
+			clean_argv(argv, count);
 		}
-		clean_args(argv);
 		if (isatty(0) == 0)
 			break;
 	}
-	exit(0);
+	return (1);
 }
 
 /**
- * setup_argv - Mallocs space for arguments in argv
- * @argv - The pointer to the pointer of argv.
- * @size - The amount of args in argv
+ * malloc_argv - Allocates memory to the argv pointer pointer
+ * @count: The amount of strings to be stored in argv
  *
- * Return: 0 on success or 1 on malloc error.
+ * Return: Pointer to a pointer to argv
  */
 
-int setup_argv(char **argv, int size)
+char **malloc_argv(int count)
 {
-	int i = 0;
+	char **args = NULL;
+	int i;
 
-	
-	while (i < size)
-	{
-		argv[i] = (char *)malloc((MAX_STRING_LENGTH + 1) * sizeof(char));
-		if (argv[i] == NULL)
-		{
-			printf("Failed to malloc argv[%d]\n", i);
-			return (1);
-		}
-		i++;
-	}
-	return (0);
+	args = (char **)malloc(count * sizeof(char *));
+	for (i = 0; i < count; i++)
+		args[i] = NULL;
+	return (args);
 }
 
 /**
- * build_args - Build the argv list from the users input.
- * @input: String the user inputted to the terminal
- * @argv: The array of string arguments to build.
+ * get_input - Gets input from the user and stores it in argv
+ * @argv: Pointer to pointer of argv
  */
 
-void build_args(char *input, char **argv)
+void get_input(char **argv)
 {
+	char *buffer = NULL;
+	size_t bsize = 100;
+	char *token = NULL;
 	int i = 0;
-	char *token;
 
-	token = strtok(input, "\n");
+	if (getline(&buffer, &bsize, stdin) == -1)
+	{
+		printf("Failed to read line\n");
+		free(buffer);
+		return;
+	}
+	token = strtok(buffer, "\n");
 	token = strtok(token, " ");
 	while (token != NULL)
 	{
-		strcpy(argv[i], token);
+		argv[i] = strdup(token);
 		token = strtok(NULL, " ");
 		i++;
 	}
-	argv[i] = NULL;
+	free(buffer);
+	free(token);
 }
 
 /**
- * clean_args - Run after the main loop to clear out the args and prepare
- * for a new set of args.
- * @args: The array of string arguments to clear.
+ * clean_argv - Frees all memory held by argv
+ * @argv: Pointer to a pointer of argv
+ * @count: The number of entries to expect in argv
  */
 
-void clean_args(char *args[])
+void clean_argv(char **argv, int count)
 {
-	int i = 0;
+	int i;
 
-	while (args[i] != NULL)
+	for (i = 0; i < count; i++)
 	{
-		args[i] = NULL;
-		i++;
+		free(argv[i]);
 	}
-	/*print_args(args);*/
-}
-
-/**
- * print_args - A utility function to check the contents of the args list,
- * used for debugging.
- * @args: The array of string arguments to print.
- */
-
-void print_args(char *args[])
-{
-	int i = 0;
-
-	while (args[i] != NULL)
-	{
-		printf("arg %d = %s\n", i, args[i]);
-		i++;
-	}
+	free(argv);
 }

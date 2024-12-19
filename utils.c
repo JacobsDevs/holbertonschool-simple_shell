2361c2dcc2f2681000fc36b86ec60ef05333934a
JacobsDevs <jacobsdevsmail@gmail.com>
2024-12-19 11:36:49 +1100

    add: utils.c

---
+++ utils.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_environ - Iterates through the environ variable and prints each one to
+ * stdout.
+ * @argv: Pointer to the argv array.
+ */
+
+void print_environ(char **argv)
+{
+	int i = 0;
+
+	while (environ[i] != NULL)
+	{
+		printf("%s\n", environ[i]);
+		i++;
+	}
+	clean_argv(argv);
+}
+
+/**
+ * run_exit - cleans the args in argv and runs exit with exit_status
+ * @argv: Pointer to argv array.
+ * @exit_status: The exit status to return to the system
+ */
+void run_exit(char **argv, int exit_status)
+{
+	clean_argv(argv);
+	exit(exit_status);
+}
